a40dd30fea1f18e6965b39fd4fcf256f7911cb5e
Giorgi Ubiria <ubiriagiorgi8@gmail.com>
2022-10-25 16:26:57 +0400

    Added user model

---
+++ src/models/userSchema.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+export interface IUser extends Document {
+  email: String
+  firstName: String
+  lastName: String
+}
+
+const UserSchema: Schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  login: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+})
+
+export default mongoose.model<IUser>("User", UserSchema)
